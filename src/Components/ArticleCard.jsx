e9cb3d6afd2918884ff51a287a703359f821bd84
Willfoss <118803774+Willfoss@users.noreply.github.com>
2024-08-09 14:23:36 +0100

    article card styling changes to stop title overlapping

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -12,10 +12,13 @@ export default function ArticleCard(props) {
   return (
     <li className="card bg-white self-stretch bg-base-100 shadow-xl max-w-[750px] p-4 m-4 dark:bg-gray-800">
       <Link to={`/articles/${article.article_id}`} article_id={article.article_id}>
-        <p className={` ${loggedInUser.username === article.author ? "text-blue-500 dark:text-blue-500" : "text-gray-700 dark:text-gray-300"}`}>
-          by: {article.author}
-        </p>
-        <h2 className="card-title text-gray-700 dark:text-gray-300">{article.title}</h2>
+        <div className="flex flex-col justify-center items-center ">
+          <p className={` ${loggedInUser.username === article.author ? "text-blue-500 dark:text-blue-500" : "text-gray-700 dark:text-gray-300"}`}>
+            by: {article.author}
+          </p>
+          <h2 className="card-title text-gray-700 dark:text-gray-300">{article.title}</h2>
+        </div>
+
         <figure>
           <img className="rounded-md " src={article.article_img_url} alt={`image for article ${article.title}`} />
         </figure>
@@ -23,7 +26,7 @@ export default function ArticleCard(props) {
       <div className="card-body flex-row justify-startitems-center">
         <ArticleVotes className="card-body flex-row justify-start items-center" article={article} />
         <p className="flex justify-end text-gray-700 dark:text-gray-300">{article.comment_count}</p>
-        <Link to={`/articles/${article.article_id}`}>
+        <Link to={`/articles/${article.artic}`}>
           <MessageCircle className="text-gray-700 dark:text-gray-300" />
         </Link>
       </div>
--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -44,15 +44,20 @@ export default function SingleArticle() {
   return (
     <div className="single-article-container flex flex-col justify-center items-center m-5">
       <li className="card bg-base-100 bg-white self-stretch max-w-[750px] shadow-xl p-4 m-2 dark:bg-gray-800">
-        <p className={` ${loggedInUser.username === singleArticle.author ? "text-blue-500 dark:text-blue-500" : "text-gray-700 dark:text-gray-300"}`}>
-          by: {singleArticle.author}
-        </p>
-        <h2 className="card-title text-gray-700 dark:text-gray-300">{singleArticle.title}</h2>
+        <div className="flex flex-col justify-center items-center ">
+          <p
+            className={` ${loggedInUser.username === singleArticle.author ? "text-blue-500 dark:text-blue-500" : "text-gray-700 dark:text-gray-300"}`}
+          >
+            by: {singleArticle.author}
+          </p>
+          <h2 className="card-title text-gray-700 dark:text-gray-300">{singleArticle.title}</h2>
+        </div>
+
         <figure>
           <img className="rounded-md" src={singleArticle.article_img_url} alt={`image for article ${singleArticle.title}`} />
         </figure>
-        <p className="text-small text-gray-700 dark:text-gray-300">Posted on {date}</p>
-        <p className="text-gray-700  dark:text-gray-300">{singleArticle.body}</p>
+        <p className="ml-2 text-small text-gray-700 dark:text-gray-300">Posted on {date}</p>
+        <p className="ml-2 text-gray-700  dark:text-gray-300">{singleArticle.body}</p>
         <div className="card-body flex-row justify-start items-center">
           <ArticleVotes className="card-body flex-row justify-start items-center" article={singleArticle} />
           <p className="flex justify-end text-gray-700  dark:text-gray-300 ">{singleArticle.comment_count + optimisticCommentCount}</p>
