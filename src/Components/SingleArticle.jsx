e1b54f035fe41aa792ec4eb7b69abb884bbbb69b
Willfoss <118803774+Willfoss@users.noreply.github.com>
2024-08-10 19:40:00 +0100

    More spacing at top of single article

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -42,7 +42,7 @@ export default function SingleArticle() {
   }
 
   return (
-    <div className="single-article-container flex flex-col justify-center items-center ">
+    <div className="single-article-container flex flex-col justify-center items-center mt-8">
       <li className="card bg-base-100 bg-white self-stretch max-w-[750px] shadow-xl p-4 m-2 dark:bg-gray-800">
         <div className="flex flex-col justify-center items-center ">
           <p
